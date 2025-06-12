1ead398a56e6f102dc255cda80af4d851fd74732
Jill328 <jwoodsnola@gmail.com>
2025-06-11 23:16:42 -0500

    started building code to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
 //import statements
+import { OneEpisode } from './data'
+import { useState } from 'react'
+import { episodeList } from './data'
+
+
 export default function App() {
-  // vvariables and javascript logic
+  const [episode] = useState (OneEpisode);
+  const [selectedEpisode, setSelectedEpisode] = useState(nul);
+//OneEpisode ((id, title, description)) {
+  
+return (
+   <div className="App">
+    {OneEpisode.map(OneEpisode id=><{episode.id} title={OneEpisode.title} description={One.Episode.description}/>)}
+   </div>
+
+export default function App() {
+  
+  // variables and javascript logic
+
   //return(JSX and Javascript)
+
+
 }
---
+++ src/OneEpisode.jsx
@@ -0,0 +1,10 @@
+function OneEpisode ((id, title, description)) {
+    return (
+        <>
+        <h1>Episode</h1>
+        <p>Title</p>
+        <p>Description</p>
+        </>
+    )
+}
+export default OneEpisode
